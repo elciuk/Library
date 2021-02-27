0d9cb3b07fafeb0e2395a073f3c8342db2ffd0f2
Wee beastie <pavel.cink5@gmail.com>
2021-02-27 16:19:59 +0000

    add basic DOM

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const addButton = document.querySelector(".add");
+const overlay = document.querySelector(".overlay");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPages = document.querySelector("#pages");
+const formRead = document.querySelector("#read");
+const formCancel = document.querySelector(".cancel");
+
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  myLibrary.push(new Book(formTitle.content, formAuthor.content, formPages.content, formRead.value));
+}
+function showOverlay(){
+
+  overlay.style.display = "block";
+}
+cancel.onclick = function(){
+  overlay.style.display = "none"
 }
