69f6c3e1bc5951fa415263a8a007d52e5a65ebe6
Wee beastie <pavel.cink5@gmail.com>
2021-03-03 13:26:07 +0000

    add function to on load loop through myLibrary and add all objects to the list

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const fConfirm = document.querySelector("#confirm")
 const fCancel = document.querySelector(".cancel");
 const list = document.querySelector(".list");
 
-let myLibrary = [];
+
+let myLibrary = [{title:"Lord of the Rings",author:"J.R.R. Tolkien",pages:123, read:"read"}, {title:"Game of Thrones", author:"G.R.R. Martin",pages:456, read: "read"}];
 function Book(title, author, pages, read) {
   // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
+for(let a=0;a<myLibrary.length;a++){
+  list.insertAdjacentHTML("beforeend",
+      `<div class="book">
+      <div class="delete" onclick="deleteEntry()">&#10060;</div>
+      <div class="index">#${a+1}</div>
+      <div class="title">${myLibrary[a].title}</div>
+      <div class="author">${myLibrary[a].author}</div>
+      <div class="pages">${myLibrary[a].pages}</div>
+      <div class="read">${myLibrary[a].read}</div>
+  </div>`);
 }
 
 function showOverlay(){
@@ -36,17 +48,24 @@ function validateForm(){
 function addBookToLibrary() {
   // called by validateForm() if all fields are valid
   // pushes new Book into myLibrary[]
-  myLibrary.push(new Book(fTitle.value, fAuthor.value, fPages.value, fRead.checked));
-  console.log(myLibrary);
+  let checkRead = "";
+  if(fRead.checked == true){
+    checkRead = "read";
+  } else {
+    checkRead = "unread";
+  }
+  myLibrary.push(new Book(fTitle.value, fAuthor.value, fPages.value, checkRead));
+
   list.insertAdjacentHTML("beforeend",
       `<div class="book">
-      <div class="delete">&#10060;</div>
+      <div class="delete" onclick="deleteEntry()">&#10060;</div>
       <div class="index">#${myLibrary.length}</div>
-      <div class="title">${fTitle.value}</div>
-      <div class="author">${fAuthor.value}</div>
-      <div class="pages">${fPages.value}</div>
-      <div class="read">${fRead.checked}</div>
+      <div class="title">${myLibrary[myLibrary.length-1].title}</div>
+      <div class="author">${myLibrary[myLibrary.length-1].author}</div>
+      <div class="pages">${myLibrary[myLibrary.length-1].pages}</div>
+      <div class="read">${myLibrary[myLibrary.length-1].read}</div>
   </div>`);
+  console.log(myLibrary);
   clearForm();
 }
 
@@ -55,4 +74,7 @@ function clearForm(){
   fAuthor.value = "";
   fPages.value = "";
   fRead.checked = false;
+}
+function deleteEntry(){
+  console.log(myLibrary);
 }
